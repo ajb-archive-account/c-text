2f715255e3a1c05b1d55fd6226bd856866699cb7
andybyers21 <andy.byers21@gmail.com>
2020-10-24 20:37:59 +0100

    enable raw mode in terminal

--- c-text.c
+++ c-text.c
@@ -1,18 +1,45 @@
+#include <stdlib.h>
 #include <termios.h>
 // Terminal I/O interface
 #include <unistd.h>
 // Access to POSIX system
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  /* Save a copy of teh termios original state in `&orig_termios` and use
+   * `tcsetattr()` to reapply it on close.
+   * `TCSAFLUSH` will clear leftover input from the shell on exit. */
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  /* Set terminal attributes, modify, pass new attributes back out to terminal.
+   * TCSAFLUSH specifies when to apply changes */
+
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
-  /* `main`, the default entry point for all c programs. When you `return` from
-   * main(), the program exits and returns an int to the operating system. (A
-   * return value of 0 indicates success). */
   /* Read 1 byte from standard input into variable `c` (and keep reading until
-   * there are no bytes left to read). */
-  /* `&& c !='q'` tell the terminal to kill the while loop when it 'hears' a 'q'
-   * keypress. */
-  /* `read()` returns the number of bytes it has read and will return 0 when
+   * there are no bytes left to read).
+   * `&& c !='q'` tell the terminal to kill the while loop when it 'hears' a 'q'
+   * keypress.
+   * `read()` returns the number of bytes it has read and will return 0 when
    * complete. */
+
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
